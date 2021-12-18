b4de3dce5638dd6c32f8bf1ae859ecad99b72063
Maoki Yoshimoto <65587647+Maoki55@users.noreply.github.com>
2021-12-18 23:05:44 +0900

    テキストエリア追加の実装

--- sample.js
+++ sample.js
@@ -1,3 +1,5 @@
-const addButtonClick = () => {
-    alert("Clicked !");
+const addButtonHandle = () => {
+    const elements = document.getElementById("inputText");
+    const copied = elements.lastElementChild.cloneNode(true);
+    elements.appendChild(copied);
 }
