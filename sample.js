aeddda5dffd0bd00a59a54884ebd863c03d83771
Maoki Yoshimoto <65587647+Maoki55@users.noreply.github.com>
2021-12-19 00:06:53 +0900

    追加・削除に上限

--- sample.js
+++ sample.js
@@ -1,10 +1,29 @@
+let textareaNum = 1;
+const className = document.getElementsByClassName("container");
+
 const addButtonHandle = () => {
+    if(textareaNum == 15) {
+        alert("これ以上増やせません");
+        exit;
+    }
     const elements = document.getElementById("inputText");
     const copied = elements.lastElementChild.cloneNode(true);
     elements.appendChild(copied);
+    for(let i=0; i < className.length; i++) {
+        className[i].setAttribute("id", "Container"+i);
+    }
+    textareaNum++;
 }
 
 const deleteButtonHandle = () => {
+    if(textareaNum == 1) {
+        alert("これ以上削除できません");
+        exit;
+    }
     const elements = document.getElementById("inputText");
-    elements.removeChild(elements.lastChild);
+    elements.removeChild(elements.children[3]); //ここの指定をどうするかが課題
+    for(let i=0; i < className.length; i++) {
+        className[i].setAttribute("id", "Container"+i);
+    }
+    textareaNum--;
 }
